4e3b0e8e88b96b9bf7dc8812e636ac82390b7b94
keerthiram2612 <keerthiram2612@gmail.com>
2023-07-14 13:25:54 +0530

    created the router of the components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from "./pages/Home"
-
+import Products from "./pages/Products"
+import SignIn from "./pages/SignIn";
+import Services from "./pages/Services";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import ForgotPassword from './pages/ForgotPassword';
 function App() {
   return (
     < >
      <Router>
       <Routes>
         <Route path="/"element={<Home/>}/>
+        <Route path="/profile"element={<Profile/>}/>
+        <Route path="/products"element={<Products/>}/>
+        <Route path="/sign-in"element={<SignIn/>}/>
+        <Route path="/sign-up"element={<SignUp/>}/>
+        <Route path="/services"element={<Services/>}/>
+        <Route path="/forgot-password"element={<ForgotPassword/>}/>
       </Routes>
      </Router>
     </>
