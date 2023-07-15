2fa394a8cb0e8e2e8069ca0cf7f0867b73007b9b
keerthiram2612 <keerthiram2612@gmail.com>
2023-07-15 20:47:45 +0530

    modifing  the signin page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-
 import { useLocation,useNavigate } from 'react-router-dom'
 
 export default function Header() {
@@ -10,27 +9,27 @@ export default function Header() {
     }
    }
   return (
-    <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
-        <header className='flex justify-between items-center px-3 max-w-6xl max-auto '>
+    <div className='bg-white border-b shadow-sm sticky top-0 z-50 '>
+        <header className='flex justify-between items-center px-3 max-w-8xl max-auto'>
             <div>
-                <img className="h-24 cursor-pointer px-0 -mt-5" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6mwlduQ2t7__zOKwZKaqzg_powboEoYo2iBKSIPH7J2YbEhW5Wnbo0JwM__51tCZTcuI&usqp=CAU" 
+                <img className="h-10 w-20  cursor-pointer px-0 -mt-5 " src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0-rwvCYTS4D029xUrilLLKk7OHY0SmS3knQ&usqp=CAU" 
                 alt="logo" onClick={()=>navigate("/")}/>
-                <h1 className={`ml-20 -mt-12 ml-16 text-lg font-semibold cursor-pointer`}>Medicure</h1>
+                <h1 className={`ml-20 -mt-11 ml-16 text-lg font-semibold cursor-pointer text-green-600`}>Medicure</h1>
             </div>
-            <div>
-                <ul className='flex space-x-5'>
-                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${pathMathRoute("/")&& "text-black border-b-red-500"}`}
+            <div className='flex '>
+                <ul className='flex space-x-5 mr-2'>
+                    <li className={`cursor-pointer  py-3 text-sm font-semibold text-gray-500 border-b-[3px] border-b-transparent ${pathMathRoute("/")&& "text-black border-b-green-600"}`}
                     onClick={()=>navigate("/")}>Home</li>
-                    <li className={` cursor-pointer py-3 text-sm  font-semibold text-gray-500 border-b-[3px] border-b-transparent ${pathMathRoute("/products")&& "text-black border-b-red-500"}`}
+                    <li className={` cursor-pointer py-3 text-sm  font-semibold text-gray-500 border-b-[3px] border-b-transparent ${pathMathRoute("/products")&& "text-black border-b-green-600"}`}
                     onClick={()=>navigate("/products")}>Products</li>
-                    <li  className={`cursor-pointer py-3 text-sm  font-semibold text-gray-500 border-b-[3px] border-b-transparent ${pathMathRoute("/resources")&& "text-black border-b-red-500"}`}
+                    <li  className={`cursor-pointer py-3 text-sm  font-semibold text-gray-500 border-b-[3px] border-b-transparent ${pathMathRoute("/resources")&& "text-black border-b-green-600"}`}
                     onClick={()=>navigate("/resources")}>Resources</li>
-                    <li  className={`cursor-pointer py-3 text-sm  font-semibold text-gray-500 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in")&& "text-black border-b-red-500"}`}
+                    <li  className={`cursor-pointer py-3 text-sm  font-semibold text-gray-500 border-b-[3px] border-b-transparent ${pathMathRoute("/sign-in")&& "text-black border-b-green-600"}`}
                     onClick={()=>navigate("/sign-in")}>Sign in</li>
                 </ul>
             </div>
         </header>
-       
+
     </div>
   )
 }
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Signin() {
-  return (
-    <div>Signin</div>
+export default function SignIn() {
+  const [formData , setFormData] = useState({
+    email : "",
+    password: "",
+  });
+  const {email,password} = formData;
+  function onchange(e){
+    setFormData((prevState)=>({
+      ...prevState,
+       []
+     
+    }) )
+  }
+    return (
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold mb-12 md:mb-6'>Sign In</h1>
+      <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl max-auto'>
+      <div className='md:w-[67%] lg:w-[50%]'>
+        <img className="w-full rounded-2xl"src="https://plus.unsplash.com/premium_photo-1679860703713-9c9f5428f652?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGtleXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="Key" />
+      </div>
+      <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20 -mb-6'>
+        <form >
+          <input className='w-full mt-6' type="email" id="email"value={email} onChange={onchange}/>
+        </form>
+      </div>
+      </div>
+    </section>
   )
 }
